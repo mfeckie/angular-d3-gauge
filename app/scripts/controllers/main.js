4d055a0878a02ccb7315634c077d6b6595c1f767
Martin Feckie <mfeckie@gmail.com>
2013-12-19 19:17:14 +0800

    Initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('angularD3GaugesApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
