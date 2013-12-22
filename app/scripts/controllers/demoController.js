7771198656d3a1c592a6ce38d67ceda2a3ad1c48
Martin Feckie <mfeckie@gmail.com>
2013-12-22 23:32:38 +0800

    Update documentation

--- app/scripts/controllers/demoController.js
+++ app/scripts/controllers/demoController.js
@@ -3,4 +3,11 @@
 angular.module('angularD3GaugesApp')
     .controller('demoCtrl', function ($scope) {
       $scope.demoValue = 50;
+      $scope.demoSections = [
+        [0, 10],
+        [10, 20],
+        [20, 30],
+        [30, 40],
+        [40, 50]
+      ];
     });
